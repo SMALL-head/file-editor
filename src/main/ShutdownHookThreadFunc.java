2e78f87bd531883596f94fb9f906ae55f454bb76
xieeryihe <2640182777@qq.com>
2023-10-24 14:05:10 +0800

    fix: 临时文件删除bug修复

--- src/main/ShutdownHookThreadFunc.java
+++ src/main/ShutdownHookThreadFunc.java
@@ -4,6 +4,8 @@ import src.context.FileEditorContext;
 import src.utils.FileEditorConstants;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 /**
  * 异常退出需要删除临时文件，这里使用钩子函数来帮忙删除
@@ -16,7 +18,16 @@ public class ShutdownHookThreadFunc implements Runnable {
         // 异常退出时删除临时文件
         String activeFile = FileEditorContext.getContext().getActiveFile();
         File file = new File(activeFile + FileEditorConstants.TMP_SUFFIX);
+        System.out.println(activeFile + FileEditorConstants.TMP_SUFFIX);
         if (file.exists()) {
+            RandomAccessFile openFile = FileEditorContext.getContext().getFile();
+            if (openFile != null) {
+                try {
+                    openFile.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
             boolean delete = file.delete();
             if (delete) {
                 System.out.println("FileEditor被关闭，但是有临时文件未保存...");
